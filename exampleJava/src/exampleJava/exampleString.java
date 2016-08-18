abd481030e5d2eb498dc977e6d1c1027ae3ee69c
dineshk9848 <dineshk9848@gmail.com>
2016-08-17 23:44:18 -0700

    initial master node with new repository testing

---
+++ exampleJava/src/exampleJava/exampleString.java
@@ -0,0 +1,17 @@
+package exampleJava;
+
+import java.util.StringTokenizer;
+
+public class exampleString {
+public static void main(String [] args){
+int i=0;
+String str = "Hello how are you. Feeling all right today on the count of riveria";
+StringTokenizer st = new StringTokenizer(str, " ");
+while (st.hasMoreTokens()) {
+	i++;
+    System.out.println(st.nextToken()); 
+}
+System.out.println(i);
+}
+}
+
